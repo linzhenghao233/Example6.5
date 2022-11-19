fe3b03fe2adcb9315c0ef5e43ac3dd89435d0da6
林政浩 <1392872316@qq.com>
2022-11-19 20:42:12 +0800

    添加项目文件。

---
+++ cmpflt.c
@@ -0,0 +1,17 @@
+/* cmpflt.c -- 浮点数比较 */
+#include <math.h>
+#include <stdio.h>
+int main(void) {
+	const double ANSWER = 3.14159;
+	double response;
+
+	printf("What is the value of pi?\n");
+	scanf_s("%lf", &response);
+	while (fabs(response - ANSWER) > 0.0001) {
+		printf("Try again!\n");
+		scanf_s("%lf", &response);
+	}
+	printf("Close enough!\n");
+
+	return 0;
+}
